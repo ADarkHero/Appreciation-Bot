b45bcc79d2f8558e51724b50e4edb8ac2db479e4
ADarkHero <maik.riedlsperger@gmail.com>
2019-05-15 11:04:48 +0200

    Add project files.

---
+++ IstHaltKeinBot/IstHaltKeinBot/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Tweetinvi;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace IstHaltKeinBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String jsonfile = "steam.json";
+
+            //If the json file is nonexistend or older than 7 days, it should be downloaded
+            var threshold = DateTime.Now.AddDays(-7);
+            var fileage = System.IO.File.GetLastWriteTime(jsonfile); //Age of the json file
+            if (!File.Exists(jsonfile) || fileage  < threshold){
+                //Downloads the latest json file of all games that exist on steam.
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadFile("http://api.steampowered.com/ISteamApps/GetAppList/v0002/", jsonfile);
+            }
+
+            //Reads the data from the steam.json
+            StreamReader sr = new StreamReader(jsonfile);
+            String jsonData = sr.ReadToEnd();
+            var outObject = JsonConvert.DeserializeObject<RootObject>(jsonData);    //Serializes the JSon String
+            sr.Close();
+
+
+
+            //Generates random numbers
+            int appNumber = outObject.applist.apps.Count;   //How many Steam-Apps?
+            Random Rnd = new Random();
+            int random1 = Rnd.Next(appNumber);  //First game
+            int random2 = Rnd.Next(appNumber);  //Second game
+
+            //Writes a new tweet
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            Tweet.PublishTweet(outObject.applist.apps[random1].name + " ist halt kein " + outObject.applist.apps[random2].name + ".");
+        }
+    }
+
+    public class App
+    {
+        public int appid { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Applist
+    {
+        public List<App> apps { get; set; }
+
+    }
+
+    public class RootObject
+    {
+        public Applist applist { get; set; }
+    }
+
+}
---
+++ IstHaltKeinBot/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Tweetinvi;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using Tweetinvi.Models;
+
+namespace AppreciationBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usersfile = "users.csv";
+            string tweetsfile = "tweets.csv";
+
+            //Read current line number from settings
+            int curLine = Properties.Settings.Default.Line;
+            curLine = 0; //Debug stuff
+            Properties.Settings.Default.Line = curLine;
+            Properties.Settings.Default.Save();
+            if (curLine == 0)
+            {
+                writeUserList();
+            }
+
+
+            //Read current user the bot should tweet to
+            string user = File.ReadLines(usersfile).Skip(curLine).Take(1).First();
+
+            //Read random tweet message
+            int messageLineCount = File.ReadLines(tweetsfile).Count();
+            Random random = new Random();
+            int randomLine = random.Next(0, messageLineCount-1);
+            string message = File.ReadLines(tweetsfile, Encoding.GetEncoding("UTF-8")).Skip(randomLine).Take(1).First();
+
+            //Writes a new tweet
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            string tweet = "@" + user + " " + message;
+            Tweet.PublishTweet(" ");
+
+            //After the tweet was successfully published, increase the line number by one
+            curLine++;
+            Properties.Settings.Default.Line = curLine;
+            Properties.Settings.Default.Save();
+        }
+
+        private static void writeUserList()
+        {
+            //Download user list
+            var userCredentials = Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            var authenticatedUser = User.GetAuthenticatedUser(userCredentials);
+            var followers = authenticatedUser.GetFollowerIds();
+
+            //Write userlist to string
+            string userlist = null;
+            for (int i = 0; i < followers.Count(); i++)
+            {
+                try
+                {
+                    userlist += User.GetUserFromId(followers.ElementAt(i)).ScreenName + "\r\n";
+                }
+                //Exception occures, if you make to many api calls. So let's wait for a bit.
+                catch(NullReferenceException ex)
+                {
+                    Console.Write(ex.ToString());
+                    System.Threading.Thread.Sleep(60000);
+                    i--;
+                }             
+            }
+
+            var followings = authenticatedUser.GetFriendIds();
+            for (int i = 0; i < followings.Count(); i++)
+            {
+                try
+                {
+                    userlist += User.GetUserFromId(followings.ElementAt(i)).ScreenName + "\r\n";
+                }
+                //Exception occures, if you make to many api calls. So let's wait for a bit.
+                catch (NullReferenceException ex)
+                {
+                    Console.Write(ex.ToString());
+                    System.Threading.Thread.Sleep(60000);
+                    i--;
+                } 
+            }
+
+            //Write userlist to file
+            System.IO.File.WriteAllText(@"users.csv", userlist);
+        }
+    }
+
+    public class App
+    {
+        public int appid { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Applist
+    {
+        public List<App> apps { get; set; }
+
+    }
+
+    public class RootObject
+    {
+        public Applist applist { get; set; }
+    }
+
+}
